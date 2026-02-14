0d3d54ba8690dfdf01b9ce1296f05892dc59a983
tigran_pogosyan <505081258@penske.com>
2026-02-14 16:34:42 +0400

    Initial commit - Spring Boot Hexagonal CQRS project

---
+++ src/main/java/com/gs/springhexagonalcqrscrudapp/SpringHexagonalCqrsCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.gs.springhexagonalcqrscrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalCqrsCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHexagonalCqrsCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gs/springhexagonalcqrscrudapp/SpringHexagonalCqrsCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.springhexagonalcqrscrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonalCqrsCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
